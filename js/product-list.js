a8fa4bb66f971c14be46b4f410c0de58e778140d
John Doe <surendra637877@gmail.com>
2023-12-13 00:14:51 +0530

    all files upload done

---
+++ js/product-list.js
@@ -0,0 +1,34 @@
+
+let productdiv = document.querySelector(".product-items-container");
+
+
+
+
+
+
+
+let rawproduct = async () => {
+    let product = await fetch("https://fakestoreapi.com/products");
+    let finalproduct = await product.json();
+    console.log(finalproduct);
+    productdiv.innerHTML = "";
+    finalproduct.forEach(element => {
+        productdiv.innerHTML += `
+            <div class="product-div">
+                <img src=${element.image} alt="">
+                <span>${element.title}</span>
+                <div class="rating-price">
+                    <div class="rating">
+                        <i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i>
+                    </div>
+                  
+                    <div class="product-div-price">
+                        ${element.price + " Rs"}
+                    </div>
+                </div>
+              </div>`
+    });
+}
+
+
+rawproduct();
